a698b98547db3adda60e9fa989493b2da4dfa5e2
chenxizhan <chenxizhan1208@qq.com>
2017-07-11 11:19:54 +0800

    初始提交
    建立项目框架
    	new file:   .classpath
    	new file:   .project
    	new file:   .settings/.jsdtscope
    	new file:   .settings/org.eclipse.jdt.core.prefs
    	new file:   .settings/org.eclipse.wst.common.component
    	new file:   .settings/org.eclipse.wst.common.project.facet.core.xml
    	new file:   .settings/org.eclipse.wst.jsdt.ui.superType.container
    	new file:   .settings/org.eclipse.wst.jsdt.ui.superType.name
    	new file:   WebRoot/META-INF/MANIFEST.MF
    	new file:   WebRoot/WEB-INF/classes/com/lbs/common/Config.class
    	new file:   WebRoot/WEB-INF/classes/com/lbs/controller/IndexController.class
    	new file:   WebRoot/WEB-INF/lib/jetty-server-8.1.8.jar
    	new file:   WebRoot/WEB-INF/lib/jfinal-3.1-bin-with-src.jar
    	new file:   WebRoot/WEB-INF/web.xml
    	new file:   WebRoot/index.html
    	new file:   src/com/lbs/common/Config.java
    	new file:   src/com/lbs/controller/IndexController.java

---
+++ src/com/lbs/common/Config.java
@@ -0,0 +1,17 @@
+package com.lbs.common;
+import com.jfinal.config.*;
+import com.jfinal.template.Engine;
+import com.lbs.controller.*;
+public class Config  extends JFinalConfig  {
+    public void configConstant(Constants me) {
+        me.setDevMode(true);
+    }
+    public void configRoute(Routes me) {
+        me.add("/", IndexController.class);
+        //me.add("/result/result.html",ResultController.class);
+    }
+    public void configEngine(Engine me) {}
+    public void configPlugin(Plugins me) {}
+    public void configInterceptor(Interceptors me) {}
+    public void configHandler(Handlers me) {}
+}
---
+++ src/com/lbs/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.lbs.controller;
+import com.jfinal.core.Controller;
+public class IndexController extends Controller {
+	public void index(){
+		render("index.html");
+	}
+}
