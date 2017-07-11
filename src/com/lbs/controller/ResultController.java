247c544f26c3b8f0f44bc61a6b98d7b8d789bc0e
chenxizhan <chenxizhan1208@qq.com>
2017-07-11 15:51:41 +0800

    引入搜索模块

--- src/com/lbs/controller/ResultController.java
+++ src/com/lbs/controller/ResultController.java
@@ -1,11 +1,14 @@
 package com.lbs.controller;
 import com.jfinal.core.Controller;
+import searchFromES.SearchFromES;;
 public class ResultController  extends Controller{
+	private static SearchFromES sfs = new SearchFromES();
 	public void index(){
 		render("");
 	}
 	public void search() {
 		String string = getPara("searchString");
+		
 		renderText(string);
 	}
 }
