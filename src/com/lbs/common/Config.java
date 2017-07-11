4569058b946aa6dd1b1b6ef5dffdffba0a7c8515
xuxiaokang <18660139963@163.com>
2017-07-11 14:50:43 +0800

    填加主页index.html

--- src/com/lbs/common/Config.java
+++ src/com/lbs/common/Config.java
@@ -10,8 +10,12 @@ public class Config  extends JFinalConfig  {
         me.add("/", IndexController.class);
         me.add("/result/result.html",ResultController.class);
     }
-    public void configEngine(Engine me) {}
+    public void configEngine(Engine me) {
+    		me.addSharedFunction("/common/_layout.html");
+		me.addSharedFunction("/common/_paginate.html");
+    }
     public void configPlugin(Plugins me) {}
     public void configInterceptor(Interceptors me) {}
     public void configHandler(Handlers me) {}
+    
 }
