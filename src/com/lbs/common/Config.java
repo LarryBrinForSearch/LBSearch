1e1365f74928ed119d8cac9e4d0da1f8d36aea75
chenxizhan <chenxizhan1208@qq.com>
2017-07-11 14:21:10 +0800

    在Config类中添加启动项目的main方法
    	modified:   src/com/lbs/common/Config.java

--- src/com/lbs/common/Config.java
+++ src/com/lbs/common/Config.java
@@ -1,5 +1,6 @@
 package com.lbs.common;
 import com.jfinal.config.*;
+import com.jfinal.core.JFinal;
 import com.jfinal.template.Engine;
 import com.lbs.controller.*;
 public class Config  extends JFinalConfig  {
@@ -14,4 +15,23 @@ public class Config  extends JFinalConfig  {
     public void configPlugin(Plugins me) {}
     public void configInterceptor(Interceptors me) {}
     public void configHandler(Handlers me) {}
+    
+	/**
+	 * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
+	 * 
+	 * 使用本方法启动过第一次以后，会在开发工具的 debug、run config 中自动生成
+	 * 一条启动配置，可对该自动生成的配置再添加额外的配置项，例如 VM argument 可配置为：
+	 * -XX:PermSize=64M -XX:MaxPermSize=256M
+	 */
+	public static void main(String[] args) {
+		/**
+		 * 特别注意：Eclipse 之下建议的启动方式
+		 */
+		JFinal.start("WebRoot", 80, "/", 5);
+
+		/**
+		 * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
+		 */
+		// JFinal.start("WebRoot", 80, "/");
+	}
 }
