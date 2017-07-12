3428be3cbf1b3833c990ba060c452ec7f115797c
xuxiaokang <18660139963@163.com>
2017-07-12 15:37:17 +0800

    添加按频道搜索功能

--- src/com/lbs/controller/ResultController.java
+++ src/com/lbs/controller/ResultController.java
@@ -9,14 +9,41 @@ import searchFromES.SearchFromES;
 public class ResultController  extends Controller{
 
 	private static String searchString=null;
+	private static String channel=null;
 	private static int page_size = 15; // 每页显示15条搜索结果
 	private static int list_size = 10; // 搜索结果可能
 	public void index(){
 		render("");
 	}
+	public void setChannel() {
+		int c= getParaToInt(0);
+		switch(c) {
+		case 1:
+			channel = "财经";
+			break;
+		case 2:
+			channel = "国际";
+			break;
+		case 3:
+			channel = "股票";
+			break;
+		case 4:
+			channel = "新闻";
+			break;
+		case 5:
+			channel = "体育";
+			break;
+		case 6:
+			channel = "军事";
+			break;
+		}
+		setAttr("channel",channel);
+		render("/index/index.html");
+	}
 	public  void search() {
 		searchString = getPara("searchString");
-		ResultModel result = Config.sfs.QueryAtFirst(searchString,null,page_size);
+		channel = getPara("channel");
+		ResultModel result = Config.sfs.QueryAtFirst(searchString,channel,page_size);
 		setAttr("result", result);
 		setAttr("page_size", page_size);
 		setAttr("current_page", 1);
@@ -27,7 +54,7 @@ public class ResultController  extends Controller{
 	public void page() {
 		int i = getParaToInt(0);
 		System.out.println("page: string is \"" + searchString + "\"\n\t page is \"" + i);;
-		ResultModel result = Config.sfs.QueryLater(i, page_size, searchString, null);
+		ResultModel result = Config.sfs.QueryLater(i, page_size, searchString, channel);
 		setAttr("result", result);
 		setAttr("page_size", page_size);
 		setAttr("current_page", i);
