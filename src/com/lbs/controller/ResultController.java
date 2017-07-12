c609e70715ad98acb78bbcc5b47984a251006c9c
chenxizhan <chenxizhan1208@qq.com>
2017-07-12 09:59:32 +0800

    修正RusultController的代码
    	modified:   src/com/lbs/controller/ResultController.java

--- src/com/lbs/controller/ResultController.java
+++ src/com/lbs/controller/ResultController.java
@@ -1,15 +1,15 @@
 package com.lbs.controller;
 import com.jfinal.core.Controller;
+
 import lsmodel.ResultModel;
 import searchFromES.SearchFromES;;
 public class ResultController  extends Controller{
+	private static SearchFromES sfs = new SearchFromES();
 	public void index(){
 		render("");
 	}
 	public void search() {
 		String string = getPara("searchString");
-		//调用search方法
-		//传到新页面
 		ResultModel result = sfs.QueryAtFirst(string,null);
 		setAttr("result", result);
 		System.out.println(result.getJsonArr().size());
