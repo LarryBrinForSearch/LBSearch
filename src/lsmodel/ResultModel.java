9b6ee9ee6c22a5062d0658b2ec3507f0a19b71e6
ChenXu1996 <892363216@qq.com>
2017-07-13 15:54:37 +0800

    完善聚合查询

--- src/lsmodel/ResultModel.java
+++ src/lsmodel/ResultModel.java
@@ -1,12 +1,16 @@
 package lsmodel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.JSONObject;
 
 public class ResultModel {
 	private long totalHit;					//总命中数
 	private ArrayList<JSONObject> jsonArr;  //结果json数组
 	private String time =null;
+	private HashMap<String,Long> staMap; //统计数据
 	
 	public String getTime() {
 		return time;
@@ -17,10 +21,18 @@ public class ResultModel {
 	public ResultModel(){
 		
 	}
-	public ResultModel(long th,ArrayList<JSONObject> arr,String times){
+	public ResultModel(long th,ArrayList<JSONObject> arr,String times,HashMap<String,Long> staMap){
 		this.totalHit = th;
 		this.jsonArr = arr;
 		this.time=times;
+		this.staMap=staMap;
+	}
+	
+	public HashMap<String, Long> getStaMap() {
+		return staMap;
+	}
+	public void setStaMap(HashMap<String, Long> staMap) {
+		this.staMap = staMap;
 	}
 	public long getTotalHit() {
 		return totalHit;
