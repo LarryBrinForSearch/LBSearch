d9a549e11838076e6fd55ae9575c44caf7926ece
chenxizhan <chenxizhan1208@qq.com>
2017-07-15 15:06:45 +0800

    重跑模块引入成功，但是没有充分测试
    	modified:   WebRoot/admin_page/dataRestore.html
    	new file:   WebRoot/admin_page/rerun_result.html
    	modified:   src/com/lbs/controller/AdminController.java
    	new file:   src/lsmodel/ReRun2.0.jar
    	new file:   src/lsmodel/StartReRunModel.java

--- src/com/lbs/controller/AdminController.java
+++ src/com/lbs/controller/AdminController.java
@@ -1,6 +1,10 @@
 package com.lbs.controller;
 
+import java.util.Date;
+
 import com.jfinal.core.Controller;
+
+import lsmodel.StartReRunModel;
 
 public class AdminController extends Controller {
 	public void index(){
@@ -10,10 +14,24 @@ public class AdminController extends Controller {
 		render("/admin_page/dataMoniter.html");
 	}
 	public void restore(){
-		String start = getPara("startInput");
-		String end = getPara("endInput");
-		System.out.println("com.lbs.conroler.AdminContreller.restore:\n\t"+start+":"+end);
+
 		render("/admin_page/dataRestore.html");
 	}
+	public void rerun(){
+		String start = getPara("startInput");
+		String end = getPara("endInput");
+		String info = null;
+		System.out.println("com.lbs.conroler.AdminContreller.restore:\n\t"+start+":"+end);
+		if (StartReRunModel.reRun(start, end)){
+			info="数据重跑成功，重跑范围是从"+start+"到" +end;
+		}else{
+			info="数据重跑失败，详细原因请查看后台日志";
+		}
+		setAttr("timestamp",(new Date()).toString());
+		setAttr("info",info);
+		render("/admin_page/rerun_result.html");
+		
+	}
+	
 
 }
---
+++ src/lsmodel/StartReRunModel.java
@@ -0,0 +1,46 @@
+package lsmodel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StartReRunModel {
+
+      
+      public static boolean reRun(String strStart,String strEnd){
+    	  boolean flag = true;
+    	  String[] test=new String[]{"java","-jar", "./ReRun2.0.jar", strStart,strEnd};
+    	  try {
+    		   Runtime runtime = Runtime.getRuntime();
+    		   Process process = runtime.exec(test);
+    		 //取得命令结果的输出流    
+               InputStream fis = process.getInputStream();   
+               //用一个读输出流类去读    
+               BufferedReader br = new BufferedReader(new InputStreamReader(fis));   
+               String line = null;   
+               //逐行读取输出到控制台    
+               while ((line = br.readLine()) != null) {   
+                   System.out.println(line);   
+               }   
+    		  } catch (IOException e) {
+    		   e.printStackTrace();
+    		   flag=false;
+    		   System.out.println("Error exec AnyQ");
+    		  }
+    	  System.out.println("rerun from " + strStart + " to " + strEnd);
+    	  return flag;
+      }
+
+  
+      
+      public static void main(String args[]){
+    	  if(args.length!=2){
+  			 System.out.print("参数错误");
+  			return;
+  		}else{
+    	  reRun(args[0],args[1]);
+  		}
+      }
+      
+}
