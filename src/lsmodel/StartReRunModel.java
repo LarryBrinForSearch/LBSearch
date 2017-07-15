2b40a5aee3e6dd0b746c5c3ac449efa1ca615f95
chenxizhan <chenxizhan1208@qq.com>
2017-07-15 15:57:01 +0800

    临时提交，重跑模块不会报错，但没能实现预期重跑效果
    	new file:   WebRoot/admin_page/assets/css/rerun_result.css
    	modified:   WebRoot/admin_page/rerun_result.html
    	modified:   src/lsmodel/StartReRunModel.java

--- src/lsmodel/StartReRunModel.java
+++ src/lsmodel/StartReRunModel.java
@@ -7,40 +7,40 @@ import java.io.InputStreamReader;
 
 public class StartReRunModel {
 
-      
-      public static boolean reRun(String strStart,String strEnd){
-    	  boolean flag = true;
-    	  String[] test=new String[]{"java","-jar", "./ReRun2.0.jar", strStart,strEnd};
-    	  try {
-    		   Runtime runtime = Runtime.getRuntime();
-    		   Process process = runtime.exec(test);
-    		 //取得命令结果的输出流    
-               InputStream fis = process.getInputStream();   
-               //用一个读输出流类去读    
-               BufferedReader br = new BufferedReader(new InputStreamReader(fis));   
-               String line = null;   
-               //逐行读取输出到控制台    
-               while ((line = br.readLine()) != null) {   
-                   System.out.println(line);   
-               }   
-    		  } catch (IOException e) {
-    		   e.printStackTrace();
-    		   flag=false;
-    		   System.out.println("Error exec AnyQ");
-    		  }
-    	  System.out.println("rerun from " + strStart + " to " + strEnd);
-    	  return flag;
-      }
+	public static boolean reRun(String strStart, String strEnd) {
+		boolean flag = true;
+		String[] test = new String[] { "java", "-jar", "ReRun2.0.jar", strStart, strEnd };
+		try {
+System.out.println("Runtime runtime = Runtime.getRuntime();");
+			Runtime runtime = Runtime.getRuntime();
+System.out.println("Process process = runtime.exec(test);");
+			Process process = runtime.exec(test);
+System.out.println("Process process = runtime.exec(test); success");
+			// 取得命令结果的输出流
+			InputStream fis = process.getInputStream();
+			// 用一个读输出流类去读
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+			String line = null;
+			// 逐行读取输出到控制台
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			flag = false;
+			System.out.println("Error exec AnyQ");
+		}
+System.out.println("rerun from " + strStart + " to " + strEnd);
+		return flag;
+	}
 
-  
-      
-      public static void main(String args[]){
-    	  if(args.length!=2){
-  			 System.out.print("参数错误");
-  			return;
-  		}else{
-    	  reRun(args[0],args[1]);
-  		}
-      }
-      
+	public static void main(String args[]) {
+		if (args.length != 2) {
+			System.out.print("参数错误");
+			return;
+		} else {
+			reRun(args[0], args[1]);
+		}
+	}
+
 }
